15116dc4c8e674f61d39920e692492e754d18f97
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-08 13:57:44 +0100

    Update c11-acm-certificatemanager.tf

--- terraform-manifests/c11-acm-certificatemanager.tf
+++ terraform-manifests/c11-acm-certificatemanager.tf
@@ -1,8 +1,7 @@
 # ACM Module - To create and Verify SSL Certificates
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
-  #version = "6.1.0"
-  version = ""
+  version = "6.1.0"
 
   domain_name  = trimsuffix(data.aws_route53_zone.mydomain.name, ".")
   zone_id      = data.aws_route53_zone.mydomain.zone_id 
